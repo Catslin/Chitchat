fe59adfffc7e1b5a14681ce4adc9f686484e9b65
Catslin <aicatslin@gmail.com>
2023-10-06 13:03:53 +0800

    User-added and default article display

--- Nodejs-Passport-Login/server.js
+++ Nodejs-Passport-Login/server.js
@@ -1,44 +1,57 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
-const flash = require('express-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
 const app = express();
 
 // Connect to MongoDB
-const dbUrl = 'mongodb://127.0.0.1:27017/chai'; // replace with your database URL
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+const dbUrl = "mongodb://127.0.0.1:27017/chai"; // replace with your database URL
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 // User schema and model
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  articles: [
+    {
+      title: { type: String, required: true,default: "Title"},
+      content: { type: String, required: true,default: "Example of definition of article content"},
+    },
+  ],
 });
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // Passport configuration
-passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    return done(null, false, { message: 'No user with that email' });
-  }
-  try {
-    if (await bcrypt.compare(password, user.password)) {
-      return done(null, user);
-    } else {
-      return done(null, false, { message: 'Password incorrect' });
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return done(null, false, { message: "No user with that email" });
+      }
+      try {
+        if (await bcrypt.compare(password, user.password)) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Password incorrect" });
+        }
+      } catch (err) {
+        return done(err);
+      }
     }
-  } catch (err) {
-    return done(err);
-  }
-}));
+  )
+);
 passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   const user = await User.findById(id);
@@ -46,55 +59,70 @@ passport.deserializeUser(async (id, done) => {
 });
 
 // Middleware
-app.set('view-engine', 'ejs');
+app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(flash());
-app.use(session({
-  secret: 'key',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
 // Routes
-app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.name });
+app.get("/", checkAuthenticated, (req, res) => {
+  User.findOne({ name: req.user.name })
+    .populate("articles")
+    .then((user) => {
+      console.log(">>>>", user.articles);
+      res.render("index.ejs", { name: req.user.name, articles: user.articles });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/login");
+    });
 });
 
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs');
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
 });
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login',
-  failureFlash: true
-}));
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs');
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
 });
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: hashedPassword
+      password: hashedPassword,
     });
     await user.save();
-    res.redirect('/login');
+    res.redirect("/login");
   } catch {
-    res.redirect('/register');
+    res.redirect("/register");
   }
 });
 
-app.delete('/logout', (req, res) => {
+app.delete("/logout", (req, res) => {
   req.logOut();
-  res.redirect('/login');
+  res.redirect("/login");
 });
 
 // Middleware functions
@@ -102,15 +130,15 @@ function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   next();
 }
 
 // Start the server
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(3000, () => console.log("Server started on port 3000"));
